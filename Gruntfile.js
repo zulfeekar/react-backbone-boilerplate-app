ca9fb34d968458e99c33e1c98e06eda8c50e3914
Florent Requin <florent.requin@gmail.com>
2014-04-11 16:00:18 +0200

    fix jsx livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function (grunt) {
         // Watches files for changes and runs tasks based on the changed files
         watch: {
             jsapp: {
-                files: ['<%= config.app %>/scripts/{,*/}*.js'],
+                files: ['<%= config.app %>/scripts/{,*/}*.{js,jsx}'],
                 tasks: ['browserify:app'],
                 options: {
                     livereload: true
